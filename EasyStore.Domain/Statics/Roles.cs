5dc32bf32601e66f5156424e6fde50e3a5877b12
BIgnatovski8264 <157275411+BIgnatovski8264@users.noreply.github.com>
2026-02-01 12:54:54 +0200

    Add files via upload

---
+++ EasyStore.Domain/Statics/Roles.cs
@@ -0,0 +1,8 @@
+namespace EasyStore.Core.StaticClasses;
+
+public static class Roles
+{
+    public const string Admin = "Admin";       
+    public const string Cashier = "Cashier";   
+    public const string Customer = "Customer"; 
+}
